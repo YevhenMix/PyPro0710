d1dd88d8ee278b57a529bf4491dcb4f3de8af3e0
OlegPC <jf2.oleg@gmail.com>
2020-11-25 21:02:27 +0200

    Приложение Маршруты

---
+++ routes/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from .models import Route
+admin.site.register(Route)
---
+++ routes/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .forms import *
+
+
+def home(request):
+    form = RouteForm()
+    return render(request, 'routes/home.html', {'form': form})
