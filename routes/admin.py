16cba44c9dae3114cce40c5fb2eeb303d87e0f78
olegnovikov <o.novikov@ciat.net.ua>
2020-12-08 11:17:23 +0200

    list_display

--- routes/admin.py
+++ routes/admin.py
@@ -6,9 +6,9 @@ from .models import Route
 class RouteAdmin(admin.ModelAdmin):
     class Meta:
         model = Route
-        fields = ('name', 'from_city', 'to_city', 'travel_times')
-        list_display = ('name', 'from_city', 'to_city', 'travel_times')
-        # list_editable = ['from_city']
+    # fields = ('name', 'from_city', 'to_city', 'travel_times')
+    list_display = ('name', 'from_city', 'to_city', 'travel_times')
+    list_editable = ['from_city']
 
 
 admin.site.register(Route, RouteAdmin)
